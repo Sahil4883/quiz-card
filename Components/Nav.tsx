204e8da5eb429c6c1e5fadfe7bb81478ec67426b
Sahil <96993695+Sahil4883@users.noreply.github.com>
2024-05-31 16:57:36 +0530

    Nav Update

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -18,6 +18,12 @@ const Nav = () => {
             >
               Log in
             </Link>
+            <Link
+              href="#"
+              className="text-white bg-blue-700 dark:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+            >
+              Sign Up
+            </Link>
 
             <button
               data-collapse-toggle="mobile-menu-2"
