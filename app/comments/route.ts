ed3f3500badd48cd1484e8cb718a3c44725dffe1
Sahil <96993695+Sahil4883@users.noreply.github.com>
2024-06-02 18:46:44 +0530

    Tried POST method

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -3,3 +3,19 @@ import comments from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
