40338025a8acb51ff55ed20aa43303d70d9b8d5d
Sahil <96993695+Sahil4883@users.noreply.github.com>
2024-06-05 22:34:12 +0530

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import { redirect } from "next/navigation";
-
 export default async function Home() {
+  /*Checking if the user is logged in or Not */
   const session = await getSession();
   if (session?.user) {
     redirect("/dashboard");
