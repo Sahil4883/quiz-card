f456228f77fc3327e48e6d8212359d042036d98a
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-01 21:51:23 +0530

    commit

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -22,6 +22,7 @@ const Nav = () => {
               <a /* <a> tag should be used in order to be safe from the dependencies clashes */
                 className="text-white bg-blue-700 dark:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
+                /* Clerk Component */
                 <SignInButton>Get Started</SignInButton>
               </a>
             )}
