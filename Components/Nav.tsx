8c226aaf637f1cc76b1fd2c1210feb29e4f819cc
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-03 20:59:09 +0530

    Added Fallback url to the signin component

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -22,7 +22,9 @@ const Nav = () => {
               <a /* <a> tag should be used in order to be safe from the dependencies clashes */
                 className="text-white bg-blue-700 dark:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
-                <SignInButton mode="modal">Get Started</SignInButton>
+                <SignInButton mode="modal" fallbackRedirectUrl="/dashboard">
+                  Get Started
+                </SignInButton>
               </a>
             )}
             <button
