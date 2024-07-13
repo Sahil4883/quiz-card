3233e880492e21a6ecd0698d5f20d4ca7f4c1755
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-13 21:35:08 +0530

    commit

--- Components/Underdevelopment.tsx
+++ Components/Underdevelopment.tsx
@@ -6,7 +6,7 @@ export const Underdevelopment = () => {
       <div className="text-center">
         <p className="text-base font-semibold text-indigo-600">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page Underdevelopment
+          Underdevelopment
         </h1>
         <p className="mt-6 text-base leading-7 text-gray-600">
           This page is underdevelopment and will be available soon. Please check
