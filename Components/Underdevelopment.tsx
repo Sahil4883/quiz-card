fe90e04ec08b8cad345f0bcfe94ac77f6aa7abad
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-14 23:18:38 +0530

    commit

--- Components/Underdevelopment.tsx
+++ Components/Underdevelopment.tsx
@@ -17,7 +17,7 @@ export const Underdevelopment = () => {
             href="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Go back home
+            Home
           </a>
           <a href="/contact" className="text-sm font-semibold text-gray-900">
             Contact <span aria-hidden="true">&rarr;</span>
