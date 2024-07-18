cf43cd0620cc6fdc17d2ce1c58f7c52acc0b602f
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-18 22:53:03 +0530

    update 2

--- app/(All Pages)/supabase/page.tsx
+++ app/(All Pages)/supabase/page.tsx
@@ -2,6 +2,7 @@
 //IDK What to do at this point
 //Learning Supabase to understand this asap
 //Didn't learn Supabase yet
+//Starting learning supabase for the same
 "use client";
 import { createClient } from "@supabase/supabase-js";
 import { useRef, useState } from "react";
