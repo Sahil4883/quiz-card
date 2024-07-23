994af61a2b82cf64af6a75e055ef2c691d1c09da
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-23 23:22:21 +0530

    trying to implement active link

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import Link from "next/link";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { useAuth } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   const user = useAuth();
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link href="/" className="flex items-center">
+          <Link href="/" className="flex item-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Quiz-App
             </span>
