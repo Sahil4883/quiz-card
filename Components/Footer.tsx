b3f3299d345c72fc4b613ee4324c50dc4ccb5b19
Sahil4883 <shaikhsahil4883@gmail.com>
2024-07-28 22:35:53 +0530

    Insrtalling DaisyUI and changing the footer with it

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,44 +1,67 @@
 import React from "react";
+import Link from "next/link";
 
 const footer = () => {
   return (
-    <div>
-      <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
-        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <a
-              href="/"
-              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+    <footer className="footer footer-center border-t-2 text-base-content rounded p-4">
+      <nav className="grid grid-flow-col gap-4">
+        <Link href="/" className="link link-hover">
+          Home
+        </Link>
+        <Link href="/features" className="link link-hover">
+          Features
+        </Link>
+        <Link href="/about" className="link link-hover">
+          About
+        </Link>
+        <Link href="contact" className="link link-hover">
+          Contact
+        </Link>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
             >
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Quiz-App
-              </span>
-            </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-              <li>
-                <a href="/about" className="hover:underline me-4 md:me-6">
-                  About
-                </a>
-              </li>
-
-              <li>
-                <a href="/contact" className="hover:underline">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <hr className="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
-            <a href="/" className="hover:underline">
-              Quiz-App
-            </a>
-            . All Rights Reserved.
-          </span>
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
         </div>
-      </footer>
-    </div>
+      </nav>
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by
+          Quiz-App Creator
+        </p>
+      </aside>
+    </footer>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
