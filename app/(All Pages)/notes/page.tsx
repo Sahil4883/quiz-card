bb93f601b500091886a2a574211963773260c649
Sahil <shaikhsahil4883@gmail.com>
2024-09-01 21:00:23 +0530

    readNotes implemend successfully with next/learn docs

--- app/(All Pages)/notes/page.tsx
+++ app/(All Pages)/notes/page.tsx
@@ -1,34 +1,16 @@
 import { useState } from "react";
 import { createNote } from "@/app/utils/supabase/data";
-export default function CreateNote() {
-  const [title, setTitle] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      await createNote(title);
-      setMessage("Note added successfully!");
-      setTitle(""); // Clear the input field after successful submission
-    } catch (error: any) {
-      setMessage(`Error: ${error.message}`);
-    }
-  };
-
+import { readNotes } from "@/app/utils/supabase/data";
+export default async function page() {
+  const readnotes = await readNotes();
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter note title"
-          required
-        />
-        <button type="submit">Add Note</button>
-      </form>
-      {message && <p>{message}</p>}
+      <h1>Notes</h1>
+      <ul>
+        {readnotes.map((note) => (
+          <li key={note.id}>{note.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
