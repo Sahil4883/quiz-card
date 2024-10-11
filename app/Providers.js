9624a34f0c86f5118fe4cec33e72bebcd1aac163
Sahil <shaikhsahil4883@gmail.com>
2024-10-11 23:14:27 +0530

    Disabled Posthog screen recording

--- app/Providers.js
+++ app/Providers.js
@@ -6,6 +6,7 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    disable_session_recording: true,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
