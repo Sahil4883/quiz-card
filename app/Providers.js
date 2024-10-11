9feab40f54b67e3261072a2c04e11b55773d8a55
Sahil <shaikhsahil4883@gmail.com>
2024-10-11 23:15:21 +0530

    adding the information for the previous commit

--- app/Providers.js
+++ app/Providers.js
@@ -6,7 +6,7 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    disable_session_recording: true,
+    disable_session_recording: true, //This disables the PostHog session recording feature
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
