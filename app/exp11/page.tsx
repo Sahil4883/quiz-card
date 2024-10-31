d9314bd4a4fa9ae59249430cfe9568507cbc3ccb
Sahil <shaikhsahil4883@gmail.com>
2024-10-31 21:34:03 +0530

    clearing imports of exp page

--- app/exp11/page.tsx
+++ app/exp11/page.tsx
@@ -1,7 +1,4 @@
-import { currentUser } from "@clerk/nextjs/server"; // Use Clerk's `currentUser` function
-import { createTodo } from "@/actions/actions"; // Import the `createTodo` function
-import prisma from "@/lib/db";
-import NewDashboard from "@/Components/TodoComponents/NewDashboard"; // Import the `NewDashboard` component
+import NewDashboard from "@/Components/TodoComponents/NewDashboard";
 
 export default async function TodoForm() {
   return (
