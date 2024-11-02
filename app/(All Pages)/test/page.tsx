aaabc8e0ccb80437156422e957cd7655fd9b6ea4
Sahil <shaikhsahil4883@gmail.com>
2024-11-03 00:34:31 +0530

    shifting the exp11 to test page for further development

--- app/(All Pages)/test/page.tsx
+++ app/(All Pages)/test/page.tsx
@@ -1,62 +1,9 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useUser } from "@clerk/nextjs";
-import createClerkSupabaseClient from "@/app/utils/supabase/createClerkSupabaseClient";
-export default function Home() {
-  const [tasks, setTasks] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [name, setName] = useState("");
+import NewDashboard from "@/Components/TodoComponents/NewDashboard";
 
-  const { user } = useUser();
-  const client = createClerkSupabaseClient();
-
-  // This `useEffect` will wait for the `user` object to be loaded before requesting
-  // the tasks for the logged in user
-  useEffect(() => {
-    if (!user) return;
-
-    async function loadTasks() {
-      setLoading(true);
-      const { data, error } = await client.from("exp").select();
-      if (!error) setTasks(data);
-      setLoading(false);
-    }
-
-    loadTasks();
-  }, [user]);
-
-  async function createTask(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    // Insert task into the "tasks" database
-    await client.from("exp").insert({
-      name,
-    });
-    window.location.reload();
-  }
-
+export default async function TodoForm() {
   return (
-    <div>
-      <h1>Tasks</h1>
-
-      {loading && <p>Loading...</p>}
-
-      {!loading &&
-        tasks.length > 0 &&
-        tasks.map((task: any) => <p>{task.name}</p>)}
-
-      {!loading && tasks.length === 0 && <p>No tasks found</p>}
-
-      <form onSubmit={createTask}>
-        <input
-          autoFocus
-          type="text"
-          name="name"
-          placeholder="Enter new task"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <button type="submit">Add</button>
-      </form>
-    </div>
+    <>
+      <NewDashboard />
+    </>
   );
 }
--- app/exp11/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewDashboard from "@/Components/TodoComponents/NewDashboard";
-
-export default async function TodoForm() {
-  return (
-    <>
-      <NewDashboard />
-    </>
-  );
-}
